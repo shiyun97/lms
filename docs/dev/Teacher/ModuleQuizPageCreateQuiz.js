07b950d23990c89458e88e99eba18cb797f2b74e
Afiqah Rashid <aficatrashid@gmail.com>
2019-10-14 00:38:32 +0800

    added create quiz configuration form

--- docs/dev/Teacher/ModuleQuizPageCreateQuiz.js
+++ docs/dev/Teacher/ModuleQuizPageCreateQuiz.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBCard, MDBCardBody, MDBCardText, MDBIcon } from "mdbreact";
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBCard, MDBIcon, MDBInputGroup } from "mdbreact";
 import { observer, inject } from 'mobx-react';
 import styled from 'styled-components';
 import ModuleSideNavigation from "./../ModuleSideNavigation";
-import { Stepper, Step, StepLabel, Button, Typography } from '@material-ui/core';
+import { Stepper, Step, StepLabel, TextField, Typography, Switch } from '@material-ui/core';
 
 @inject('dataStore')
 @observer
@@ -19,7 +19,13 @@ class ModuleQuizPageCreateQuiz extends Component {
         instructions: "",
         quizTitle: "",
         activeStep: 0,
-        steps: ['Quiz Configuration', 'Build Quiz', 'Marking Scheme']
+        steps: ['Quiz Configuration', 'Build Quiz'],
+        quizType: "",
+        shuffle: true,
+        noOfAttempts: 1,
+        timeLimit: 60,
+        openingDate: "",
+        closingDate: ""
     }
 
     initPage() {
@@ -31,17 +37,23 @@ class ModuleQuizPageCreateQuiz extends Component {
         }
     }
 
+    handleSwitchChange = () => {
+        this.setState({
+            shuffle: !this.state.shuffle
+        });
+        console.log(this.state.shuffle)
+    }
+
     componentDidMount() {
         this.initPage();
     }
 
     handleSubmit = () => {
+        // call api to submit quiz
 
-    }
-
-    handleReset = () => {
+        // to reset
         this.setState({ activeStep: 0 });
-    };
+    }
 
     handleNext = () => {
         const currStep = this.state.activeStep;
@@ -66,29 +78,109 @@ class ModuleQuizPageCreateQuiz extends Component {
                         <h4 className="text-center">
                             Quiz Configuration
                     </h4>
-                        <ul className="list-unstyled example-components-list">
-                            <form onSubmit={this.checkLogIn} name="quiz-form" onSubmit={() => { this.handleSubmit() }}>
-                                <br />
-                                <label className="grey-text">
-                                    Quiz Title
+                        <br />
+                        <label className="grey-text">
+                            Quiz Title
                     </label>
-                                <input type="text" name="quizTitle" onChange={this.handleChange} className="form-control" />
-                                <br />
-                                <label className="grey-text">
-                                    Instructions
+                        <input type="text" name="quizTitle" onChange={this.handleChange} className="form-control" />
+                        <br />
+                        <label className="grey-text">
+                            Instructions
                     </label>
-                                <input type="text" name="instructions" onChange={this.handleChange} className="form-control" />
-                                <div className="text-center mt-4">
-                                    <MDBBtn type="submit" color="blue">Submit</MDBBtn>
-                                </div>
-                            </form>
-                        </ul>
+                        <textarea type="text" rows="3" name="instructions" onChange={this.handleChange} className="form-control" />
+                        <MDBRow>
+                            <MDBCol md="9" className="mt-4">
+                                <MDBInputGroup
+                                    style={{ paddingTop: 22 }}
+                                    containerClassName="mb-3"
+                                    prepend="Quiz Type"
+                                    required
+                                    inputs={
+                                        <select name="quizType" onChange={this.handleChange} className="browser-default custom-select">
+                                            <option value="0">Choose...</option>
+                                            <option value="Normal">Normal</option>
+                                            <option value="Adaptive">Adaptive</option>
+                                        </select>
+                                    }
+                                />
+                            </MDBCol>
+                            <MDBCol md="3" className="mt-4" style={{ paddingTop: 20 }}>
+                                <label className="grey-text">
+                                    Shuffle Questions
+                    </label>
+                                <Switch checked={this.state.shuffle} onChange={() => this.handleSwitchChange()} color="primary" name="shuffle" value={this.state.shuffle} />
+                            </MDBCol>
+                            <MDBCol md="6">
+                                <br />
+                                <TextField
+                                    id="datetime-local"
+                                    label="Opening Date"
+                                    type="datetime-local"
+                                    name="openingDate"
+                                    value={this.state.openingDate}
+                                    onChange={this.handleChange}
+                                    fullWidth
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                />
+                            </MDBCol>
+                            <MDBCol md="6">
+                                <br />
+                                <TextField
+                                    id="datetime-local"
+                                    label="Closing Date"
+                                    type="datetime-local"
+                                    name="closingDate"
+                                    value={this.state.closingDate}
+                                    onChange={this.handleChange}
+                                    fullWidth
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                />
+                            </MDBCol>
+                            <MDBCol md="6" className="mt-4">
+                                <label className="grey-text">
+                                    Time Limit (in minutes)
+                    </label>
+                                <input type="number" className="form-control" name="timeLimit"
+                                    value={this.state.timeLimit}
+                                    onChange={this.handleChange}
+                                    min={30}
+                                    required />
+                            </MDBCol>
+                            <MDBCol md="6" className="mt-4">
+                                <label className="grey-text">
+                                    Number of Attempts
+                    </label>
+                                <input type="number" className="form-control" name="noOfAttempts" onChange={this.handleChange}
+                                    value={this.state.noOfAttempts}
+                                    onChange={this.handleChange}
+                                    min={1}
+                                    required />
+                            </MDBCol>
+                        </MDBRow>
                     </div>
                 );
             case 1:
-                return 'What is an ad group anyways?';
-            case 2:
-                return 'This is the bit I really care about!';
+                return (
+                    <div style={{ padding: 60 }}>
+                        <h4 className="text-center">
+                            Build Quiz
+                    </h4>
+                        <br />
+                        <label className="grey-text">
+                            Quiz Title
+                    </label>
+                        <input type="text" name="quizTitle" onChange={this.handleChange} className="form-control" />
+                        <br />
+                        <label className="grey-text">
+                            Instructions
+                    </label>
+                        <textarea type="text" rows="3" name="instructions" onChange={this.handleChange} className="form-control" />
+                    </div>
+                );
             default:
                 return 'Unknown stepIndex';
         }
@@ -112,36 +204,48 @@ class ModuleQuizPageCreateQuiz extends Component {
                         <MDBRow className="py-3">
                             <MDBCol md="12">
                                 <MDBCard cascade className="grey lighten-4" style={{ padding: 50 }}>
-                                    <Stepper activeStep={activeStep} alternativeLabel>
-                                        {steps.map(label => (
-                                            <Step key={label}>
-                                                <StepLabel>{label}</StepLabel>
-                                            </Step>
-                                        ))}
-                                    </Stepper>
-                                    <div>
-                                        {activeStep === steps.length ? (
+                                    <ul className="list-unstyled example-components-list">
+                                        <form name="quiz-form" onSubmit={() => { this.handleSubmit() }}>
+                                            <Stepper activeStep={activeStep} alternativeLabel>
+                                                {steps.map(label => (
+                                                    <Step key={label}>
+                                                        <StepLabel>{label}</StepLabel>
+                                                    </Step>
+                                                ))}
+                                            </Stepper>
                                             <div>
-                                                <Typography>All steps completed</Typography>
-                                                <Button onClick={() => this.handleReset()}>Reset</Button>
-                                            </div>
-                                        ) : (
-                                                <div>
-                                                    <Typography>{this.getStepContent(activeStep)}</Typography>
-                                                    <div>
-                                                        <Button
-                                                            disabled={activeStep === 0}
-                                                            onClick={() => this.handleBack()}
-                                                        >
-                                                            Back
-                                                        </Button>
-                                                        <Button variant="contained" color="primary" onClick={() => this.handleNext()}>
-                                                            {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-                                                        </Button>
+                                                {activeStep === steps.length ? (
+                                                    <div style={{ padding: 60 }}>
+                                                        <h5 className="text-center">
+                                                            You have completed all steps to create the quiz. <br />
+                                                            Ensure that all fields are correct before submitting.
+                                                        </h5>
+                                                        <br />
+                                                        <div className="text-center">
+                                                            <MDBBtn type="submit" color="blue">Submit</MDBBtn>
+                                                        </div>
                                                     </div>
-                                                </div>
-                                            )}
-                                    </div>
+                                                ) : (
+                                                        <div>
+                                                            <Typography>{this.getStepContent(activeStep)}</Typography>
+                                                            <div align="center">
+                                                                <MDBBtn
+                                                                    disabled={activeStep === 0}
+                                                                    onClick={() => this.handleBack()}
+                                                                    color="grey"
+                                                                >
+                                                                    Back
+                                                                </MDBBtn>
+                                                                &nbsp;&nbsp;&nbsp;&nbsp;
+                                                                <MDBBtn color="blue darken-4" onClick={() => this.handleNext()}>
+                                                                    {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                                                                </MDBBtn>
+                                                            </div>
+                                                        </div>
+                                                    )}
+                                            </div>
+                                        </form>
+                                    </ul>
                                 </MDBCard>
                             </MDBCol>
                         </MDBRow>
