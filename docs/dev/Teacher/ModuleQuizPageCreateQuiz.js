3e8312be6294aa9f99f38f52da5f42615aa3c39c
Afiqah Rashid <aficatrashid@gmail.com>
2019-10-13 23:43:34 +0800

    create quiz stepper

--- docs/dev/Teacher/ModuleQuizPageCreateQuiz.js
+++ docs/dev/Teacher/ModuleQuizPageCreateQuiz.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBCard, MDBCardBody, MDBCardText } from "mdbreact";
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBCard, MDBCardBody, MDBCardText, MDBIcon } from "mdbreact";
 import { observer, inject } from 'mobx-react';
 import styled from 'styled-components';
 import ModuleSideNavigation from "./../ModuleSideNavigation";
+import { Stepper, Step, StepLabel, Button, Typography } from '@material-ui/core';
 
 @inject('dataStore')
 @observer
@@ -14,7 +15,11 @@ class ModuleQuizPageCreateQuiz extends Component {
         userId: "",
         email: "",
         moduleId: 0,
-        message: ""
+        message: "",
+        instructions: "",
+        quizTitle: "",
+        activeStep: 0,
+        steps: ['Quiz Configuration', 'Build Quiz', 'Marking Scheme']
     }
 
     initPage() {
@@ -34,7 +39,63 @@ class ModuleQuizPageCreateQuiz extends Component {
 
     }
 
+    handleReset = () => {
+        this.setState({ activeStep: 0 });
+    };
+
+    handleNext = () => {
+        const currStep = this.state.activeStep;
+        this.setState({ activeStep: currStep + 1 });
+    };
+
+    handleBack = () => {
+        const currStep = this.state.activeStep;
+        this.setState({ activeStep: currStep - 1 });
+    };
+
+    handleChange = event => {
+        event.preventDefault();
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    getStepContent = (stepIndex) => {
+        switch (stepIndex) {
+            case 0:
+                return (
+                    <div style={{ padding: 60 }}>
+                        <h4 className="text-center">
+                            Quiz Configuration
+                    </h4>
+                        <ul className="list-unstyled example-components-list">
+                            <form onSubmit={this.checkLogIn} name="quiz-form" onSubmit={() => { this.handleSubmit() }}>
+                                <br />
+                                <label className="grey-text">
+                                    Quiz Title
+                    </label>
+                                <input type="text" name="quizTitle" onChange={this.handleChange} className="form-control" />
+                                <br />
+                                <label className="grey-text">
+                                    Instructions
+                    </label>
+                                <input type="text" name="instructions" onChange={this.handleChange} className="form-control" />
+                                <div className="text-center mt-4">
+                                    <MDBBtn type="submit" color="blue">Submit</MDBBtn>
+                                </div>
+                            </form>
+                        </ul>
+                    </div>
+                );
+            case 1:
+                return 'What is an ad group anyways?';
+            case 2:
+                return 'This is the bit I really care about!';
+            default:
+                return 'Unknown stepIndex';
+        }
+    }
+
     render() {
+        const { steps, activeStep } = this.state;
         return (
             <div className={this.props.className}>
                 <ModuleSideNavigation moduleId={this.props.match.params.moduleId}></ModuleSideNavigation>
@@ -43,31 +104,44 @@ class ModuleQuizPageCreateQuiz extends Component {
                         <MDBRow style={{ paddingTop: 60 }}>
                             <MDBCol md="12">
                                 <h2 className="font-weight-bold">
-                                    Create Quiz
-                    </h2>
+                                    <a href="/modules/:moduleId/quiz">Quiz</a>
+                                    <MDBIcon icon="angle-right" className="ml-4 mr-4" /> Create Quiz
+                                </h2>
                             </MDBCol>
                         </MDBRow>
                         <MDBRow className="py-3">
                             <MDBCol md="12">
-                                <MDBCard cascade className="my-3 grey lighten-4">
-                                    <form name="quiz-form" onSubmit={() => { this.handleSubmit() }}>
-                                        {/* <Field
-	        name="quizTitle"
-	        type="text"
-	        component={this.renderInputField}
-	        label="Quiz Title"
-	      />
-        <Field
-	        name="quizSynopsis"
-	        type="text"
-	        component={this.renderTextareaField}
-	        label="Quiz Synopsis"
-	      />
-	      <FieldArray name="questions" component={this.renderQuestions} /> */}
-                                        <div>
-                                            <MDBBtn type="submit" color="blue">Submit</MDBBtn>
-                                        </div>
-                                    </form>
+                                <MDBCard cascade className="grey lighten-4" style={{ padding: 50 }}>
+                                    <Stepper activeStep={activeStep} alternativeLabel>
+                                        {steps.map(label => (
+                                            <Step key={label}>
+                                                <StepLabel>{label}</StepLabel>
+                                            </Step>
+                                        ))}
+                                    </Stepper>
+                                    <div>
+                                        {activeStep === steps.length ? (
+                                            <div>
+                                                <Typography>All steps completed</Typography>
+                                                <Button onClick={() => this.handleReset()}>Reset</Button>
+                                            </div>
+                                        ) : (
+                                                <div>
+                                                    <Typography>{this.getStepContent(activeStep)}</Typography>
+                                                    <div>
+                                                        <Button
+                                                            disabled={activeStep === 0}
+                                                            onClick={() => this.handleBack()}
+                                                        >
+                                                            Back
+                                                        </Button>
+                                                        <Button variant="contained" color="primary" onClick={() => this.handleNext()}>
+                                                            {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                                                        </Button>
+                                                    </div>
+                                                </div>
+                                            )}
+                                    </div>
                                 </MDBCard>
                             </MDBCol>
                         </MDBRow>
